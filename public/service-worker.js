b263618356e555098ba8ef89006fed3360c2be6e
Jon Deavers <jondeavers@gmail.com>
2020-12-30 01:10:07 -0500

    Created manifest and connected serv-worker

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map((size) => `/icons/icon-${size}x${size}.png`);
+
+const staticFilesToCache = [
+    "/",
+    "/index.js",
+    "/favicon.ico",
+    "/manifest.webmanifest",
+].concat(iconFiles);
+
